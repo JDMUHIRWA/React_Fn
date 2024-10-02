00ccf2feeff4724a0a8fbb285bb04b738af4e78d
JDMUHIRWA <j.hareriman@alustudent.com>
2024-10-02 02:07:42 +0200

    setup react env, and created an env

---
+++ react_fn/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './components/Navbar';
+import Filter from './components/Filter';
+
+const App = () => {
+  console.log('Heroh')
+  return (
+    <div>
+      <NavBar/>
+      <Filter/>
+    </div>
+  )
+}
+
+export default App
